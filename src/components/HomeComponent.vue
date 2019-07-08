ce2a6211b31ae1064646574c6dc00fd3b5a6a0b7
Jansen <v.jansen.martin@gmail.com>
2019-07-08 10:28:28 -0700

    Create form; Log input to console

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="row justify-content-center">
+ <div>
+     <p>Hello</p>
+     <form @submit.prevent="addLocation">
+         <label>Location:</label>
+         <input type="text" class="form-control" v-model="query.location">
+         <div class="form-group">
+          <button class="btn btn-primary">Submit</button>
+        </div>
+     </form>
+    </div>
+  <!-- <div class="row justify-content-center">
       <div class="col-md-8">
           <div class="card card-default">
               <div class="card-header">Home Component</div>
@@ -9,9 +19,20 @@
               </div>
           </div>
       </div>
-  </div>
+  </div> -->
 </template>
 <script>
 export default {
+    data(){
+        return {
+            query:{}
+        }
+    },
+    methods: {
+        addLocation(){
+            // eslint-disable-next-line
+            console.log(this.query.location);
+        }
+    }
 }
 </script>
