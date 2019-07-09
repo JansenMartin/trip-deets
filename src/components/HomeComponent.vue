56a76f4c87f2130441c997cb99ad52f17ecc12ac
Jansen <v.jansen.martin@gmail.com>
2019-07-09 12:48:01 -0700

    Make sure I can parse Date into an acceptable format

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -35,37 +35,38 @@ export default {
             /* eslint-disable */
             // console.log(this.query);
             console.log(this.query.date);
-            const parsedDate = Date.parse(this.query.date);
+            let parsedDate = Date.parse(this.query.date);
+            parsedDate = parsedDate / 10000
             console.log(parsedDate);
 
             const locationKey = process.env.VUE_APP_LOCATION;
             const skyKey = process.env.VUE_APP_SKY;
             let getLocationURL = 'https://us1.locationiq.com/v1/search.php';
 
-            // this.axios.get(getLocationURL, {
-            // params: {
-            //   key: locationKey,
-            //   q: this.query.location,
-            //   format: "json"
-            // }
-            // })
-            // .then((response) => {
-            //     console.log("Latitude:");
-            //     console.log(response.data[0].lat);
-            //     console.log("Longitude:");
-            //     console.log(response.data[0].lon);
-            //     this.axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${skyKey}/48.8566969,2.3514616`)
-            //     .then((reverse) => {
-            //         console.log("Inside the nested call");
-            //         console.log("Daily:")
-            //         console.log(reverse.data.daily);
-            //         console.log("Currently:")
-            //         console.log(reverse.data.currently);
-            //     })
-            // })
-            // .catch((error) => {
-            //     console.log(error);
-            // })
+            this.axios.get(getLocationURL, {
+            params: {
+              key: locationKey,
+              q: this.query.location,
+              format: "json"
+            }
+            })
+            .then((response) => {
+                console.log("Latitude:");
+                console.log(response.data[0].lat);
+                console.log("Longitude:");
+                console.log(response.data[0].lon);
+                this.axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${skyKey}/48.8566969,2.3514616`)
+                .then((reverse) => {
+                    console.log("Inside the nested call");
+                    console.log("Daily:")
+                    console.log(reverse.data.daily);
+                    console.log("Currently:")
+                    console.log(reverse.data.currently);
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
 
         }
     }
