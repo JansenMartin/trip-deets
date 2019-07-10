74f9eac770a2678a8ac94c233a2637b3e9b57f46
Jansen <v.jansen.martin@gmail.com>
2019-07-10 09:36:48 -0700

    Label 'from' and 'until' dates inside query object

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,11 +1,11 @@
 <template>
  <div>
      <p>Hello</p>
-     <form @submit.prevent="getWeather">
+     <form @submit.prevent="parseDateRange">
          <label>Location:</label>
          <input type="text" class="form-control" v-model="query.location">
          <label>From:</label>
-         <input type="text" class="form-control" v-model="query.date">
+         <input type="text" class="form-control" v-model="query.from">
          <label>Until:</label>
          <input type="text" class="form-control" v-model="query.until">
          <div class="form-group">
@@ -38,45 +38,44 @@ export default {
     },
     methods: {
         getWeather(){
-            console.log(this.query.until);
-            // const date = this.query.date;
-            // const location = this.query.location;
+            const date = this.query.date;
+            const location = this.query.location;
 
-            // // Set date to UNIX time (in a format Dark Sky accepts)
-            // let parsedDate = Date.parse(this.query.date);
-            // parsedDate = parsedDate / 10000
+            // Set date to UNIX time (in a format Dark Sky accepts)
+            let parsedDate = Date.parse(this.query.date);
+            parsedDate = parsedDate / 10000
 
-            // // API Keys
-            // const locationKey = process.env.VUE_APP_LOCATION;
-            // const skyKey = process.env.VUE_APP_SKY;
+            // API Keys
+            const locationKey = process.env.VUE_APP_LOCATION;
+            const skyKey = process.env.VUE_APP_SKY;
 
-            // // NESTED API CALLS (LocationIQ and Dark Sky)
-            // // Find latitude and longitude for a given place
-            // const getLocationURL = 'https://us1.locationiq.com/v1/search.php';
-            // this.axios.get(getLocationURL, {
-            // params: {
-            //   key: locationKey,
-            //   q: location,
-            //   format: "json"
-            // }
-            // })
-            // .then((response) => {
+            // NESTED API CALLS (LocationIQ and Dark Sky)
+            // Find latitude and longitude for a given place
+            const getLocationURL = 'https://us1.locationiq.com/v1/search.php';
+            this.axios.get(getLocationURL, {
+            params: {
+              key: locationKey,
+              q: location,
+              format: "json"
+            }
+            })
+            .then((response) => {
       
-            //     const lat = response.data[0].lat;
-            //     const lon = response.data[0].lon;
-            //     const tempProxy = 'https://cors-anywhere.herokuapp.com/';
-            //     const getWeatherURL = `${tempProxy}https://api.darksky.net/forecast/${skyKey}/${lat},${lon},${parsedDate}`;
+                const lat = response.data[0].lat;
+                const lon = response.data[0].lon;
+                const tempProxy = 'https://cors-anywhere.herokuapp.com/';
+                const getWeatherURL = `${tempProxy}https://api.darksky.net/forecast/${skyKey}/${lat},${lon},${parsedDate}`;
 
-            //     // Get weather based on latitude and longitude
-            //     this.axios.get(getWeatherURL)
-            //     .then((weather) => {
+                // Get weather based on latitude and longitude
+                this.axios.get(getWeatherURL)
+                .then((weather) => {
 
-            //         this.isolateConditions(weather);
-            //     })
-            // })
-            // .catch((error) => {
-            //     console.log(error);
-            // })
+                    this.isolateConditions(weather);
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
 
         },
         isolateConditions(weather) {
@@ -96,28 +95,19 @@ export default {
 
         },
         parseDateRange() {
-          // This needs to return an array of UNIX times!!
 
-        //   const dateRange = eachDay(
+        console.log("INSIDE PARSEDATERANGE:");
+        console.log(this.query.from);
+        console.log(this.query.until);
+
+        // const unixDates = eachDay(
         //     new Date(2014, 9, 6),
         //     new Date(2014, 9, 10)
-        //   )
+        //   ).map((date) =>  {
+        //       return Date.parse(date);
+        //   });
 
-        //   unixTimes = dateRange.map
-
-        const unixDates = eachDay(
-            new Date(2014, 9, 6),
-            new Date(2014, 9, 10)
-          ).map((date) =>  {
-              return Date.parse(date);
-          });
-
-          console.log(unixDates);
-
-        //   console.log(eachDay(
-        //     new Date(2014, 9, 6),
-        //     new Date(2014, 9, 10)
-        //   ));
+        //   console.log(unixDates);
 
         }
     }
