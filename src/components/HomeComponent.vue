0446c42d6ca8fc1a65cc6de556149f712c16764e
Jansen <v.jansen.martin@gmail.com>
2019-07-11 09:36:30 -0700

    Remove comments

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -127,11 +127,5 @@ export default {
 <style>
 .btn-primary {
     margin-top: 1em;
-  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px; */
 }
 </style>
