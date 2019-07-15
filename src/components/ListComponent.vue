fba6aade1e1087a586f107726e30e9dc5b9aba8c
Jansen <v.jansen.martin@gmail.com>
2019-07-15 07:58:01 -0700

    Add items to item object

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -12,17 +12,19 @@ export default {
   },
   data(){
     return {
-      list: []
+      list: ["1 jacket"]
     }
   },
   methods: {
     generateList() {
         const items = {
             general: ["pairs of socks", "sets of underwear"],
-            cold: [],
-            hot: [],
-            snowy: [],
-            rainy: []
+            cold: ["long-sleeved shirt"],
+            coldGeneral: ["coat", "hat", "gloves/mittens", "sweater"],
+            hotGeneral: ["summer hat"],
+            hot: ["t-shirt", "shorts"],
+            snowy: ["umbrella", "boots"],
+            rainy: ["raincoat", "umbrella"],
         }
 
         // Add general daily items to the packing list (based on number of days)
@@ -30,8 +32,33 @@ export default {
             this.list.push(`${this.weatherData.length} ${items.general[i]}`);
         }
 
-        // this.list.push(`${this.weatherData.length} ${items.general[0]}`);
-        // this.list.push(`${this.weatherData.length} ${items.general[1]}`);
+        // Add other misc items (like a jacket)
+
+        // Initialize number of cold days
+        // Initialize number of hot days
+        // Initialize number of temperate days
+        
+        // Initialize rainy as FALSE
+        // Initialize snowy as FALSE
+
+        // Iterate over the weather data, increment number of days based on HIGH
+        // Check rainy TRUE if rainy day appears
+        // Check snowy TRUE if snowy day appears
+
+        // If number of cold days is GREATER THAN 0...
+            // Add hat, heavy coat, gloves/mittens to the list
+            // Add long-sleeved shirts (based on the number of COLD days)
+
+        // If number of hot days is GREATER THAN 0...
+            // Add summer hat and sandals to the list
+            // Add t-shirts (based on number of days)
+            // Add shorts (based number of HOT days / 2)
+
+        // If rainy AND snowy are TRUE (OR) snowy ONLY is true
+            // Add umbrella and snowboots
+        // else (only rainy is true)
+            // Add umbrella and raincoat
+        
     }
   },
 }
