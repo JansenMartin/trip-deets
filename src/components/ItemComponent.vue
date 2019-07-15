eee09eaaf59b72733df1df51714a98a942d7e042
Jansen <v.jansen.martin@gmail.com>
2019-07-15 15:18:51 -0700

    Add ability to mark item as complete

--- src/components/ItemComponent.vue
+++ src/components/ItemComponent.vue
@@ -1,17 +1,28 @@
 <template>
-  <li>
+  <li @click="toggle" v-bind:class="{ complete: isComplete }">
     {{item}}
   </li>
 </template>
 
 <script>
+/* eslint-disable */
   export default {
     props: ['item'],
     data () {
       return {
         isComplete: false
+      }     
+    },
+    methods: {
+      toggle() {
+          this.isComplete = ! this.isComplete
       }
-        
     }
   }
 </script>
+
+<style>
+  .complete {
+    text-decoration: line-through;
+  }
+</style>
