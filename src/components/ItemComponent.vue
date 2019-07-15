6e7af7dfed80fb7992ddc307f7af89850ad9ef94
Jansen <v.jansen.martin@gmail.com>
2019-07-15 14:40:30 -0700

    Add state to ItemComponent

--- src/components/ItemComponent.vue
+++ src/components/ItemComponent.vue
@@ -1,9 +1,17 @@
 <template>
-<p></p>
+  <div class="{ complete: isComplete }">
+
+  </div>
 </template>
 
 <script>
   export default {
     props: ['item'],
+    data () {
+      return {
+        isComplete: false
+      }
+        
+    }
   }
 </script>
