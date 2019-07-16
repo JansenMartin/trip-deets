ede3fc1f13b1cd646c32e50a6a667c6090e0c990
Jansen <v.jansen.martin@gmail.com>
2019-07-16 16:02:07 -0700

    Build and print summary array above the packing list

--- src/components/SummaryComponent.vue
+++ src/components/SummaryComponent.vue
@@ -1,5 +1,10 @@
 <template>
-<p>{{  summary }}</p>
+<!-- eslint-disable  -->
+<div>
+  <ul v-for="item in summary">
+      <li>{{ item }}</li>
+    </ul>
+</div>
 </template>
 
 <script>
@@ -13,12 +18,49 @@ export default {
   data() {
     // Stuff goes here
     return {
-      summary: "It's a summary!"
+      summary: []
     }
   },
   methods: {
     buildSummary() {
-      this.summary = `Built a summary!  Got some props: ${this.weatherData[0].icon}`;
+    
+      let rain = false;
+      let snow = false;
+      let hottestTemp = this.weatherData[0].high;
+
+      for (let i = 0; i < this.weatherData.length; i += 1) {
+          const weather = this.weatherData[i];
+
+          if (weather.icon === "rain") {
+              rain = true;
+            //   this.summary += "Expect rain.  ";
+          }
+          else if (weather.icon === "snow" || weather.icon === "sleet") {
+              snow = true;
+            //   this.summary += "Expect snowy weather.  ";
+          }
+          
+          if (weather.high > hottestTemp) {
+              hottestTemp = weather.high
+            //   this.summary += ` The warmest day will be ${hottestTemp}F.  `
+          }
+        }
+
+      if (rain && snow) {
+        this.summary.push("Expect rain and snow during your stay. ");
+      }
+      else if (rain && !snow) {
+        this.summary.push("Expect rain during your stay.  ");
+      }
+      else if (!rain && snow) {
+        this.summary.push("Expect snow during your stay.  ");
+      }
+      else {
+        this.summary.push("Well, hey!  Looks pretty clear!  ");
+      }
+
+      this.summary.push(`Your warmest day will be ${hottestTemp}F.`);  
+      this.summary.push(`We recommend taking these things with you: `);
     }
   }
 }
