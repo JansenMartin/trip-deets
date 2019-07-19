07299a638c47a2bb9e0a38e7aa884e358fe21d03
Jansen <v.jansen.martin@gmail.com>
2019-07-19 10:33:15 -0700

    Add deployed proxy server URL for Dark Sky API call

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -59,7 +59,7 @@ export default {
 
             // API Keys
             const locationKey = process.env.VUE_APP_LOCATION;
-            const skyKey = process.env.VUE_APP_SKY;
+            // const skyKey = process.env.VUE_APP_SKY;
 
             // NESTED API CALLS (LocationIQ and Dark Sky)
             // Find latitude and longitude for a given place
@@ -81,8 +81,8 @@ export default {
 
                 for (let i = 0; i < unixDates.length; i += 1) {
                     // const getWeatherURL = `${tempProxy}https://api.darksky.net/forecast/${skyKey}/${lat},${lon},${unixDates[i]}`;
-                    const getWeatherURL = `http://localhost:3000/api/v1/json`;
-                    // const getWeatherURL = `https://whispering-everglades-96543.herokuapp.com/api/v1/json`;
+                    // const getWeatherURL = `http://localhost:3000/api/v1/json`;
+                    const getWeatherURL = `https://whispering-everglades-96543.herokuapp.com/api/v1/json`;
                     // Get weather based on latitude and longitude
                     this.axios.get(getWeatherURL, {
                       params: {
